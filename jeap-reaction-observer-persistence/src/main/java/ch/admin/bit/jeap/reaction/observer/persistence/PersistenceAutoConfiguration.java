193b95dfddca5d1080dc0fe372e85fd0836f11cd
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2025-05-23 15:55:31 +0200

    JEAP-5491 Remove caching
    Little benefit, leads to issues with false negative cache hits

--- jeap-reaction-observer-persistence/src/main/java/ch/admin/bit/jeap/reaction/observer/persistence/PersistenceAutoConfiguration.java
+++ jeap-reaction-observer-persistence/src/main/java/ch/admin/bit/jeap/reaction/observer/persistence/PersistenceAutoConfiguration.java
@@ -1,23 +1,16 @@
 package ch.admin.bit.jeap.reaction.observer.persistence;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.concurrent.TimeUnit;
-
 @AutoConfiguration
 @EnableTransactionManagement
 @EnableJpaRepositories
 @EntityScan
-@EnableCaching
 class PersistenceAutoConfiguration {
 
     @Bean
@@ -33,19 +26,5 @@ class PersistenceAutoConfiguration {
     @Bean
     ObservedReactionsAggregatedRepositoryImpl observedReactionsAggregatedRepository(JdbcTemplate jdbcTemplate) {
         return new ObservedReactionsAggregatedRepositoryImpl(jdbcTemplate);
-    }
-
-    @Bean
-    public Caffeine<Object, Object> caffeineConfig() {
-        return Caffeine.newBuilder()
-                .maximumSize(10_000)
-                .expireAfterWrite(24, TimeUnit.HOURS);
-    }
-
-    @Bean
-    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCaffeine(caffeine);
-        return cacheManager;
     }
 }
